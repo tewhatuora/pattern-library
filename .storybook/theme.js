a66975e8f27a8dd686e2ef6bf6ac179250cc0008
Kyle Beattie <kyle.beattie@alphero.com>
2022-04-12 13:55:16 +1200

    Configure Storybook Branding theme

---
+++ .storybook/theme.js
@@ -0,0 +1,36 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Ministry of Health',
+    brandUrl: 'https://example.com',
+    brandImage: './Logo.svg',
+
+    colorPrimary: '#002E6E',
+    colorSecondary: '#0071BC',
+
+    // UI
+    appBg: '#E5E5E5',
+    appContentBg: '#fff',
+    appBorderColor: '#7E94B5',
+    appBorderRadius: 8,
+
+    // Typography
+    fontBase: '"Fira Sans", sans-serif',
+    fontCode: 'monospace',
+
+    // Text colors
+    textColor: '#002E6E',
+    textInverseColor: 'rgba(255,255,255,0.9)',
+
+    // Toolbar default and active colors
+    barTextColor: '#fff',
+    barSelectedColor: '#fff',
+    barBg: '#002E6E',
+
+    // Form colors
+    inputBg: 'white',
+    inputBorder: '#7E94B5',
+    inputTextColor: '#002E6E',
+    inputBorderRadius: 8,
+});
