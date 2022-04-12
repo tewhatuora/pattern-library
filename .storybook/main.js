79ec3c208ee4b8fe9cfeb455f716a17303d6afb5
Kyle Beattie <kyle.beattie@alphero.com>
2022-04-12 13:25:43 +1200

    Configure storybook to read stories from `lib/components/**`
    - Add addons
    - Add Vanilla Extract webpack plugin

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../lib/src/components/**/*.stories.mdx',
+    '../lib/src/components/**/*.docs.mdx',
+    '../lib/src/components/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@react-theming/storybook-addon'
+  ],
+  framework: '@storybook/react',
+  staticDirs: ['./public'],
+  webpackFinal: async (config, { configType }) => {
+    // Add Vanilla Extract here
+    config.plugins = [...config.plugins, new VanillaExtractPlugin()]
+    return config
+  },
+}
