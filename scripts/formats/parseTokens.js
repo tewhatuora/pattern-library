44ca133cf244683f027742d9b97f71d517cd3e6f
Kyle Beattie <kyle.beattie@alphero.com>
2022-05-04 17:04:56 +1200

    WIP parse typography tokens

--- scripts/formats/parseTokens.js
+++ scripts/formats/parseTokens.js
@@ -1,22 +1,14 @@
-const StyleDictionary = require('style-dictionary');
-const prettier = require('prettier');
-
 const { simplifyTokens, trim } = require('../utils');
 
-const parseTokens = ({ dictionary, file, options }) => {
+const parseTokens = ({ dictionary, options }) => {
   const {
     color: colorTokens,
-    space: spaceTokens,
+    spacing: spaceTokens,
     effect: effectTokens,
     typography: typographyTokens,
     grid: gridTokens,
     ...otherTokens
   } = simplifyTokens(dictionary.tokens);
-
-  // const {
-  //   typography: typeSizeTokens,
-  //   ...otherSizeTokens
-  // } = sizeTokens;
 
   const color = Object.keys(colorTokens).reduce((acc, key) => {
     for (const subkey of Object.keys(colorTokens[key])) {
@@ -37,31 +29,98 @@ const parseTokens = ({ dictionary, file, options }) => {
     return acc;
   }, {});
 
-  // const fontSizes = Object.keys(typeSizeTokens)
-  //   .reduce((acc, key) => ({
-  //     ...acc,
-  //     [key]: typeSizeTokens[key].font,
-  //   }), {});
-  //
-  // const lineHeights = Object.keys(typeSizeTokens)
-  //   .reduce((acc, key) => ({
-  //     ...acc,
-  //     [key]: typeSizeTokens[key].line,
-  //   }), {});
+
+
+
+
+  const sizeMap = {
+    xs: 'xsmall',
+    s: 'small',
+    m: 'standard',
+    l: 'large',
+    xl: 'xlarge',
+  };
+
+  const makeSize = (size) => {
+    if (size in sizeMap) {
+      return sizeMap[size];
+    }
+
+    const regex = /^(\d?)x([s|l])$/g;
+    const extras = regex.exec(size);
+
+    if (extras.length) {
+      return `${Array(Number(extras[1])).fill('x').join('')}${sizeMap[extras[2]]}`;
+    }
+  };
+
+  /*
+  Convert typography tokens
+   */
+  const typography = Object.keys(typographyTokens).reduce(
+    (tokens, bp) => {
+      for (const size of Object.keys(typographyTokens[bp])) {
+        for (const weight of Object.keys(typographyTokens[bp][size])) {
+          tokens.fontWeight[weight] = typographyTokens[bp][size][weight].fontWeight;
+          const formattedSize = makeSize(size);
+
+          tokens.text[formattedSize] = {
+            [bp]: {
+              fontSize: typographyTokens[bp][size][weight].fontSize,
+              lineHeight: typographyTokens[bp][size][weight].lineHeight,
+            },
+          };
+        }
+      }
+
+      return tokens;
+    },
+    {
+      fontFamily: '',
+      fontWeight: {},
+      heading: {},
+      text: {},
+    },
+  );
+
+
+  console.log(typographyTokens, typography);
 
   return {
     name: options.name,
     displayName: options.displayName,
-    typography: typographyTokens,
-    space: {},
-    transitions: {},
-    border: {},
+    typography,
+    // space: spaceTokens,
+    space: {
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      6: '1.5rem',
+      8: '2rem',
+      10: '2.5rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      18: '4.5rem',
+      20: '5rem',
+    },
+    transitions: {
+      fast: 'transform .125s ease, opacity .125s ease',
+    },
+    border: {
+      radius: {
+        standard: '8px',
+        large: '12px',
+        xlarge: '16px',
+      },
+      width: {
+        standard: '1px',
+      },
+    },
     shadows,
     grid: gridTokens,
     color,
-
-    // fontSizes,
-    // lineHeights,
     // ...otherSizeTokens,
     // ...otherTokens,
   };
