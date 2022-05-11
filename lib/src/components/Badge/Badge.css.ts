e355851859316fa45b01d303d19382c4083aa847
Kyle Beattie <kyle.beattie@alphero.com>
2022-05-11 13:17:56 +1200

    Badge component WIP

---
+++ lib/src/components/Badge/Badge.css.ts
@@ -0,0 +1,72 @@
+import { style } from '@vanilla-extract/css';
+import { RecipeVariants, recipe } from '@vanilla-extract/recipes';
+
+import { responsiveStyle } from '../../css/responsiveStyle';
+
+import { atoms } from '../../css/atoms/atoms';
+
+import { vars } from '../../themes/vars.css';
+
+const variant = {
+  positive: atoms({
+    color: 'positive0',
+    backgroundColor: {
+      base: 'positive100',
+    },
+  }),
+  info: atoms({
+    color: 'info0',
+    backgroundColor: {
+      base: 'info100',
+    },
+  }),
+  neutral: atoms({
+    color: 'neutral100',
+    backgroundColor: {
+      base: 'neutral5',
+    },
+  }),
+  caution: atoms({
+    color: 'caution0',
+    backgroundColor: {
+      base: 'caution100',
+    },
+  }),
+  critical: atoms({
+    color: 'error0',
+    backgroundColor: {
+      base: 'error100',
+    },
+  }),
+};
+
+export type Variant = keyof typeof variant;
+
+export const variants = recipe({
+  base: style([
+    atoms({
+      display: 'inlineFlex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 'standard',
+    }),
+    responsiveStyle({
+      mobile: {
+        height: '2.2rem',
+        paddingLeft: vars.space.xsmall.mobile,
+        paddingRight: vars.space.xsmall.mobile,
+      },
+      tablet: {
+        height: '2.6rem',
+        paddingLeft: vars.space.xsmall.tablet,
+        paddingRight: vars.space.xsmall.tablet,
+      },
+    }),
+  ]),
+  variants: {
+    variant,
+  },
+  compoundVariants: [],
+});
+
+export type Variants = RecipeVariants<typeof variants>;
---
+++ lib/src/components/Badge/Badge.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import { Text } from '../Text/Text';
+
+import { Box } from '../Box/Box';
+import * as styles from './Badge.css';
+
+export type BadgeProps = {
+  variant: styles.Variant;
+} & Pick<
+  JSX.IntrinsicElements['button'],
+  'onClick' | 'onMouseEnter' | 'onMouseLeave' | 'children' | 'disabled' | 'type' | 'tabIndex'
+>;
+
+/**
+ * Badge component
+ * @param props
+ * @constructor
+ */
+export const Badge = ({ variant = 'neutral', children, ...boxProps }: BadgeProps) => {
+  return (
+    <Box
+      aria-role="status"
+      as="div"
+      className={styles.variants({
+        variant,
+      })}
+      // Passed-through boxProps.
+      // Note: Default values for boxProps need to be
+      // assigned after the {...boxProps} spread below
+      {...boxProps}
+    >
+      <Text size="xsmall" weight="bold">
+        {children}
+      </Text>
+    </Box>
+  );
+};
+
+Badge.displayName = 'Badge';
+// export default Badge;
