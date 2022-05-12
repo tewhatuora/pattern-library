5448069c54ebe29809da307aecfeec62eb70f843
Kyle Beattie <kyle.beattie@alphero.com>
2022-05-12 17:22:50 +1200

    Make svg icons work with webpack & storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const path = require('path');
+
+const pathToInlineSvg = path.resolve(__dirname, '../lib/src/icons');
 
 module.exports = {
   stories: [
@@ -14,10 +17,31 @@ module.exports = {
     '@react-theming/storybook-addon'
   ],
   framework: '@storybook/react',
+  core: {
+    'builder': '@storybook/builder-webpack5'
+  },
   staticDirs: ['./public'],
   webpackFinal: async (config, { configType }) => {
     // Add Vanilla Extract here
-    config.plugins = [...config.plugins, new VanillaExtractPlugin()]
-    return config
+    config.plugins = [...config.plugins, new VanillaExtractPlugin()];
+
+
+    // modify storybook's file-loader rule to avoid conflicts with svgr
+    const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'));
+    fileLoaderRule.exclude = pathToInlineSvg;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      include: pathToInlineSvg,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+        },
+      }],
+    });
+
+
+    return config;
   },
 }
