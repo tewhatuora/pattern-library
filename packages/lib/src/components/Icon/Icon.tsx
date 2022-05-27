74d36cf8f2da21d0fd1ca5297e56e8dddc008e6c
Kyle Beattie <kyle.beattie@alphero.com>
2022-05-27 16:26:46 +1200

    Cleanup icon types

--- packages/lib/src/components/Icon/Icon.tsx
+++ packages/lib/src/components/Icon/Icon.tsx
@@ -3,12 +3,12 @@ import clsx from 'clsx';
 
 import { Box, BoxProps } from '../Box/Box';
 import * as styles from './Icon.css';
-import icons from './icons';
+import icons, { IconType } from './icons';
 
-export const validIcons = Object.keys(icons) as const;
+const validIcons = Object.keys(icons);
 
 type IconProps = {
-  icon: keyof typeof icons;
+  icon: IconType;
   variant?: styles.Variant;
   onClick?: () => void;
   className?: string;
@@ -22,12 +22,13 @@ type IconProps = {
  */
 export const Icon = ({ icon, className, variant = 'decorativeIcons', ...boxProps }: IconProps) => {
   assert(
-    validIcons.includes(icon),
+    !!icon && validIcons.includes(icon),
     `Invalid Icon component: '${icon}'. Should be one of [${validIcons.map((c) => `'${c}'`).join(', ')}]`,
   );
 
   const IconComponent = icons[icon];
 
+  // TODO: fix typescript issue with <IconComponent />
   return (
     <Box as="span" className={clsx(styles.icon, styles.variants({ variant }), className)} {...boxProps}>
       <IconComponent />
