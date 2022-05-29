b5c4bc53ed432675ebd4885c1707df3fdc576553
Chris Meekin <chris.meekin@alphero.com>
2022-05-30 08:59:48 +1200

    Add Card css

---
+++ packages/lib/src/components/Card/Card.css.ts
@@ -0,0 +1,31 @@
+import { style } from '@vanilla-extract/css';
+import { RecipeVariants, recipe } from '@vanilla-extract/recipes';
+
+import { atoms } from '../../css/atoms/atoms';
+
+import { vars } from '../../themes/vars.css';
+
+export const variants = recipe({
+  base: style([
+    atoms({
+      backgroundColor: 'neutral0',
+      borderRadius: 'card',
+    }),
+  ]),
+  variants: {
+    /**
+     * The variant is in here because getting the variant type was converting
+     * the boolean `true` into the string `'true'`. It wasn't helping so I've
+     * just placed it directly in here and made the prop a boolean in
+     * `Card.tsx`.
+     */
+    hasShadow: {
+      true: {
+        boxShadow: vars.shadow['10'],
+      },
+    },
+  },
+  compoundVariants: [],
+});
+
+export type Variants = RecipeVariants<typeof variants>;
