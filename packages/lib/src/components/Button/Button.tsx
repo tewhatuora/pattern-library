f7a61cd58d1049d4fe41e909278bf63f56acaca2
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-01 11:03:48 +1200

    Fix Button types, remove `label` prop

--- packages/lib/src/components/Button/Button.tsx
+++ packages/lib/src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ElementType, forwardRef, Ref, RefObject } from 'react';
+import { ElementType, forwardRef, ReactText, Ref, RefObject, PropsWithChildren } from 'react';
 import { useButton } from '@react-aria/button';
 import clsx from 'clsx';
 
@@ -18,38 +18,36 @@ type KeysUnder<T, K extends PropertyKey> = T extends object
 type IconVariant = KeysUnder<styles.Variants, 'icon'>;
 type ColorVariant = KeysUnder<styles.Variants, 'color'>;
 
-export type ButtonProps = {
+export type ButtonType<Props> = {
   icon?: IconType;
   iconPosition?: IconVariant;
   as?: ElementType;
   className?: string;
-  label?: string;
   variant?: ColorVariant;
   onPress?: (e: any) => void;
-} & Pick<JSX.IntrinsicElements['button'], 'children' | 'disabled' | 'type' | 'tabIndex'> &
+} & Props &
+  Pick<JSX.IntrinsicElements['button'], 'disabled' | 'type' | 'tabIndex'> &
   Pick<BoxProps, 'width' | 'justifyContent'>;
 
-export const ButtonRoot = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement> | null) => {
-  const { children, label, as = 'button', type = 'button', className, icon, onPress, ...boxProps } = props;
+type ButtonRootProps = PropsWithChildren<ButtonType<{}>>;
+
+export type ButtonProps = ButtonType<{ children: ReactText }>;
+
+export const ButtonRoot = forwardRef((props: ButtonRootProps, ref: Ref<HTMLButtonElement> | null) => {
+  const { children, as = 'button', type = 'button', disabled, className, icon, onPress, ...boxProps } = props;
 
   const { buttonProps } = useButton(
     {
-      ...props,
+      type,
+      onPress,
+      isDisabled: disabled,
       elementType: as,
     },
     ref as RefObject<HTMLButtonElement>,
   );
 
   return (
-    <Box
-      as={as}
-      className={clsx(styles.root, className)}
-      ref={ref}
-      type={type}
-      {...buttonProps}
-      {...boxProps}
-      aria-label={label}
-    >
+    <Box as={as} className={clsx(styles.root, className)} ref={ref} type={type} {...buttonProps} {...boxProps}>
       {children}
     </Box>
   );
@@ -64,7 +62,6 @@ export const Button = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement
     as = 'button',
     type = 'button',
     variant = 'primary',
-    label,
     disabled,
     className,
     icon,
@@ -85,7 +82,6 @@ export const Button = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement
       )}
       ref={ref}
       type={type}
-      label={label}
       disabled={disabled}
       onPress={onPress}
       {...boxProps}
