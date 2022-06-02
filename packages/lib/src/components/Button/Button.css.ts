7c530a66976843ce9689295960d154799f403c7e
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-03 09:08:02 +1200

    Test incorrect import order

--- packages/lib/src/components/Button/Button.css.ts
+++ packages/lib/src/components/Button/Button.css.ts
@@ -1,8 +1,8 @@
-import { style } from '@vanilla-extract/css';
-import { RecipeVariants, recipe } from '@vanilla-extract/recipes';
-
-import { responsiveStyle } from '../../css/responsiveStyle';
 import { atoms } from '../../css/atoms/atoms';
+import { style } from '@vanilla-extract/css';
+import { responsiveStyle } from '../../css/responsiveStyle';
+
+import { RecipeVariants, recipe } from '@vanilla-extract/recipes';
 import { vars } from '../../themes/vars.css';
 
 export const root = style({
