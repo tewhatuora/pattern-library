1122b756ec5e4b3c4ca0b6840d50b6e8848e92f8
Chris Meekin <chris.meekin@alphero.com>
2022-06-02 15:27:00 +1200

    Add children to Accordion Storybook

--- packages/lib/src/components/Accordion/Accordion.stories.tsx
+++ packages/lib/src/components/Accordion/Accordion.stories.tsx
@@ -1,4 +1,5 @@
 import { Accordion, AccordionProps } from './Accordion';
+import { Text } from '../Text/Text';
 
 export default {
   title: 'Components/Accordion',
@@ -12,4 +13,17 @@ Default.args = {
   icon: 'tick',
   header: 'Header',
   label: 'Label',
+  children: (
+    <>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus gravida neque, id luctus augue aliquam a.
+        Aliquam libero massa, laoreet quis magna nec, vulputate fermentum nibh. Maecenas sit amet lobortis ipsum. Ut in
+        lacus metus. Donec id finibus mi.
+      </Text>
+      <Text>
+        Ut eget elementum eros, ut auctor lacus. Ut non congue felis. Proin iaculis accumsan diam nec hendrerit.
+        Phasellus ultricies lectus nibh, sed luctus urna ultricies sed.
+      </Text>
+    </>
+  ),
 };
