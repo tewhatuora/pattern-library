759295dee1a4cf184c541d8512221a201e160634
Chris Meekin <chris.meekin@alphero.com>
2022-06-02 15:26:46 +1200

    Add children and update icon size

--- packages/lib/src/components/Accordion/Accordion.tsx
+++ packages/lib/src/components/Accordion/Accordion.tsx
@@ -3,6 +3,7 @@ import * as RadixAccordion from '@radix-ui/react-accordion';
 
 import { Text } from '../Text/Text';
 import { Icon } from '../Icon/Icon';
+import { PropsWithChildren } from 'react';
 
 export type AccordionProps = {
   // icon?: IconType;
@@ -17,7 +18,14 @@ export type AccordionProps = {
  * Accordion
  * @constructor
  */
-export const Accordion = ({ icon, header, label, divider = 'none', variant = 'dark' }: AccordionProps) => {
+export const Accordion = ({
+  icon,
+  header,
+  label,
+  divider = 'none',
+  variant = 'dark',
+  children,
+}: PropsWithChildren<AccordionProps>) => {
   return (
     <RadixAccordion.Root type="single" collapsible className={styles.root({ divider, variant })}>
       <RadixAccordion.Item value="item-1">
@@ -25,8 +33,8 @@ export const Accordion = ({ icon, header, label, divider = 'none', variant = 'da
           <RadixAccordion.Trigger className={styles.trigger}>
             <div className={styles.headerContainer}>
               {!!icon && <Icon icon={icon} variant="functionalIcons" className={styles.icon} />}
+
               <div className={styles.headingText}>
-                {/* TODO: Change from 'p' */}
                 {!!header && (
                   <Text as="div" weight="bold">
                     {header}
@@ -36,13 +44,12 @@ export const Accordion = ({ icon, header, label, divider = 'none', variant = 'da
               </div>
             </div>
 
-            <Icon icon="chevron_down" className={styles.chevron} />
+            <Icon icon="chevron_down" variant="functionalIcons" className={styles.chevron} />
           </RadixAccordion.Trigger>
         </RadixAccordion.Header>
+
         <RadixAccordion.Content className={styles.content}>
-          <div className={styles.contentWrapper}>
-            <Text>Hello this is some content</Text>
-          </div>
+          <div className={styles.contentWrapper}>{children}</div>
         </RadixAccordion.Content>
       </RadixAccordion.Item>
     </RadixAccordion.Root>
