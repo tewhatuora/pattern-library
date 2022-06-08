ff0952e0b255c3368710f255c40b6bfb51b0101b
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-08 15:09:24 +1200

    Refactored RadioGroup to render children RadioButton components

--- packages/lib/src/components/RadioGroup/RadioGroup.stories.tsx
+++ packages/lib/src/components/RadioGroup/RadioGroup.stories.tsx
@@ -1,22 +1,52 @@
 import { RadioGroup, RadioGroupProps } from './RadioGroup';
+import { RadioButton } from './RadioButton';
 
 export default {
   title: 'Components/RadioGroup',
   component: RadioGroup,
   argTypes: {
-    labels: {
-      defaultValue: ['Content One', 'Two', 'Three'],
-    },
-    labelsWithHeading: {
-      defaultValue: [
-        { heading: 'Heading 1', label: 'One' },
-        { heading: 'Heading 2', label: 'Two' },
-        { heading: 'Heading 3', label: 'Three' },
-      ],
+    error: {
+      control: { type: 'boolean' },
     },
   },
 };
 
 export const Default = (args: RadioGroupProps) => {
-  return <RadioGroup {...args} />;
+  return (
+    <RadioGroup {...args}>
+      <RadioButton id="value_one" label="Content one" value="one" />
+      <RadioButton id="value_two" label="Content two" value="two" />
+      <RadioButton id="value_three" label="Content three" value="three" />
+    </RadioGroup>
+  );
+};
+
+export const Disabled = (args: RadioGroupProps) => {
+  return (
+    <RadioGroup {...args} value="one" onChange={(value) => alert(value)}>
+      <RadioButton disabled id="value_one" label="Content one" value="one" />
+      <RadioButton disabled id="value_two" label="Content two" value="two" />
+      <RadioButton disabled id="value_three" label="Content three" value="three" />
+    </RadioGroup>
+  );
+};
+
+export const Headings = (args: RadioGroupProps) => {
+  return (
+    <RadioGroup {...args}>
+      <RadioButton heading="Heading one" id="value_one" label="Content one" value="one" />
+      <RadioButton heading="Heading two" id="value_two" label="Content two" value="two" />
+      <RadioButton heading="Heading three" id="value_three" label="Content three" value="three" />
+    </RadioGroup>
+  );
+};
+
+export const OnChange = (args: RadioGroupProps) => {
+  return (
+    <RadioGroup {...args} onChange={(value) => alert(value)}>
+      <RadioButton heading="Heading one" id="value_one" label="Content one" value="one" />
+      <RadioButton heading="Heading two" id="value_two" label="Content two" value="two" />
+      <RadioButton heading="Heading three" id="value_three" label="Content three" value="three" />
+    </RadioGroup>
+  );
 };
--- packages/lib/src/components/RadioGroup/RadioGroup.tsx
+++ packages/lib/src/components/RadioGroup/RadioGroup.tsx
@@ -1,99 +1,49 @@
-import { ReactNode } from 'react';
-
+import { Children, PropsWithChildren, cloneElement } from 'react';
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
 
-import { Label } from '@radix-ui/react-label';
-import { Text } from '../Text/Text';
-import { Box } from '../Box/Box';
+import { RadioButton } from './RadioButton';
 
-import * as styles from './RadioGroup.css';
+import assert from 'assert';
 
 export type RadioGroupProps = {
-  labels: string[];
-  isHeadingDisplayed: boolean;
-  labelsWithHeading: { heading: string; label: string }[];
   name: string;
-  isRequired: boolean;
+  value?: string;
+  required: boolean;
+  error?: boolean | string;
   onChange?: (value: string) => void;
-  children: ReactNode;
-  state: styles.Variant;
 };
 
 /**
  * RadioGroup Component
  * Displays a list of vertically stacked radio components
  * Enables users to pick exactly one option from a radio group
- *Have an option to display a heading
+ * Have an option to display a heading
  * Have an option to be required
  *
  * @param props
  * @constructor
  */
-
 export const RadioGroup = ({
-  labels,
-  isHeadingDisplayed,
-  labelsWithHeading,
   name,
-  isRequired,
-  state = 'default',
+  value,
+  required,
+  error,
   onChange,
-  ...boxProps
-}: RadioGroupProps) => {
-  return (
-    <Box as="div" {...boxProps}>
-      <RadioGroupPrimitive.Root aria-label="View density" name={name} required={isRequired} onValueChange={onChange}>
-        {!!isHeadingDisplayed
-          ? labelsWithHeading.map(({ heading, label }) => (
-              <Box
-                className={styles.variants({
-                  variant: state,
-                })}
-              >
-                <Box className={styles.offsetButton}>
-                  <RadioGroupPrimitive.Item
-                    className={styles.styledItem}
-                    value={label}
-                    id={label}
-                    disabled={state === 'disabled'}
-                  >
-                    <RadioGroupPrimitive.Indicator className={styles.styledIndicator} />
-                  </RadioGroupPrimitive.Item>
-                </Box>
-                <Box className={styles.radioText}>
-                  <Text size="medium" weight="bold">
-                    <Label htmlFor={label}>{heading}</Label>
-                  </Text>
-                  <Text size="medium" weight="regular">
-                    {label}
-                  </Text>
-                </Box>
-              </Box>
-            ))
-          : labels.map((label) => (
-              <Box
-                className={styles.variants({
-                  variant: state,
-                })}
-              >
-                <Box className={styles.offsetButton}>
-                  <RadioGroupPrimitive.Item
-                    className={styles.styledItem}
-                    value={label}
-                    id={label}
-                    disabled={state === 'disabled'}
-                  >
-                    <RadioGroupPrimitive.Indicator className={styles.styledIndicator} />
-                  </RadioGroupPrimitive.Item>
-                </Box>
-                <Text className={styles.radioText} size="medium" weight="regular">
-                  <Label htmlFor={label}>{label}</Label>
-                </Text>
-              </Box>
-            ))}
-      </RadioGroupPrimitive.Root>
-    </Box>
-  );
-};
+  children,
+}: PropsWithChildren<RadioGroupProps>) => (
+  <RadioGroupPrimitive.Root name={name} required={required} onValueChange={onChange}>
+    {Children.map(children, (child) => {
+      assert(
+        child.type.name === RadioButton.name,
+        'Only RadioButton components are allowed as children of RadioGroup.',
+      );
+
+      return cloneElement(child, {
+        selected: child.props.value === value,
+        error: child.props.error || error,
+      });
+    })}
+  </RadioGroupPrimitive.Root>
+);
 
 RadioGroup.displayName = 'RadioGroup';
