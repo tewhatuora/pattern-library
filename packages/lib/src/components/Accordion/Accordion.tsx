72d22e9d2c8af8dd706107b84b35bc20eb49d222
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-14 04:51:40 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- packages/lib/src/components/Accordion/Accordion.tsx
+++ packages/lib/src/components/Accordion/Accordion.tsx
@@ -59,14 +59,14 @@ const Item = ({ value, children }: AccordionItemProps) => {
 export type AccordionRootProps = {
   type: 'single' | 'multiple';
   variant?: 'light' | 'dark';
-  children: typeof Item;
 };
 
+This is causing a typescript error and expecting only a single child. Can we do something like in the [RadioGroup component](https://gitlab.com/alphero/moh-design-system/-/merge_requests/22/diffs#1f13c70d2429235328c3b6254d8a8d9972300963_0_32) ? (See below)
 /**
  * Accordion
  * @constructor
  */
-const Root = ({ type, variant = 'dark', children }: AccordionRootProps) => {
+const Root = ({ type, variant = 'dark', children }: PropsWithChildren<AccordionRootProps>) => {
   return (
     <RadixAccordion.Root type={type} collapsible className={styles.root[variant]}>
       {Children.map(children, (child) => {
