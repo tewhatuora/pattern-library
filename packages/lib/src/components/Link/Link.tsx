bd448be4f4238c06dd52ff87053130b07cb21053
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-16 16:59:15 +1200

    Created a `Link` component for internal use

---
+++ packages/lib/src/components/Link/Link.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren } from 'react';
+import clsx from 'clsx';
+
+import { UseTextProps, useText } from '../../hooks/typography';
+
+import * as styles from './Link.css';
+
+export const LinkStyles = styles;
+
+type LinkProps = {
+  href: string;
+  size?: UseTextProps['size'];
+  weight?: UseTextProps['weight'];
+};
+
+/**
+ * Anchor link to a URL
+ * @param props
+ * @constructor
+ */
+export const Link = ({ href, size = 'medium', weight = 'regular', children }: PropsWithChildren<LinkProps>) => {
+  const textStyles = useText({ weight, size });
+
+  return (
+    <a className={clsx(textStyles, styles.link)} href={href}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
