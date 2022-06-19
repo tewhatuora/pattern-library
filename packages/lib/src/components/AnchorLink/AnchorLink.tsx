50f4cafeb8845f79c60e9b5e4f26cbf6d0c94f66
Kyle Beattie <kyle.beattie@alphero.com>
2022-06-20 11:43:45 +1200

    Render ReactElement or a tag

--- packages/lib/src/components/AnchorLink/AnchorLink.tsx
+++ packages/lib/src/components/AnchorLink/AnchorLink.tsx
@@ -1,27 +1,46 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement, cloneElement, createElement } from 'react';
+import clsx from 'clsx';
 
 import { Icon } from '../Icon/Icon';
-import { Text } from '../Text/Text';
 
 import * as styles from './AnchorLink.css';
+import { UseTextProps, useText } from '../../hooks/typography';
 
 export const AnchorLinkStyles = styles;
 
 export type AnchorLinkProps = {
-  href: string;
+  to: string;
+  component?: string | ReactElement;
+  size?: UseTextProps['size'];
+  weight?: UseTextProps['weight'];
 };
 
 /**
  * Links to a specified anchor point within the same page.
  * @constructor
  */
-export const AnchorLink = ({ href, children }: PropsWithChildren<AnchorLinkProps>) => {
-  return (
-    <Text as="a" className={styles.link} href={href}>
-      <Icon className={styles.icon} icon="chevron_right" variant="functionalIcons" />
-      {children}
-    </Text>
-  );
+export const AnchorLink = ({
+  to,
+  size = 'medium',
+  weight = 'regular',
+  component = 'a',
+  children,
+}: PropsWithChildren<AnchorLinkProps>) => {
+  const textStyles = useText({ weight, size });
+  const props = {
+    className: clsx(textStyles, styles.link),
+    href: to,
+  };
+  const linkChildren = [
+    <Icon className={styles.icon} icon="chevron_right" key="icon" variant="functionalIcons" />,
+    children,
+  ];
+
+  if (typeof component === 'string') {
+    return createElement(component, props, linkChildren);
+  }
+
+  return cloneElement(component, props, linkChildren);
 };
 
 AnchorLink.displayName = 'AnchorLink';
