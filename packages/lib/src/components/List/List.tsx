0522ac2d56398f9b333e1439dca846963021884a
Chris Meekin <chris.meekin@alphero.com>
2022-06-21 16:12:16 +1200

    Create empty List files

---
+++ packages/lib/src/components/List/List.tsx
@@ -0,0 +1,10 @@
+/**
+ * List is mainly a wrapper that renders a list of items.
+ * <ul> items will be passed as children
+ * Dev can choose what the list type will be
+ *   Will this be by using an <ul> vs <ol>? What about the ones without bullets or numbers?
+ */
+
+export const List = () => {
+  return <li className="" />;
+};
