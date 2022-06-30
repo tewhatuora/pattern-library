166ed79a292300806650ce1070547c932dba2660
Chris Meekin <chris.meekin@alphero.com>
2022-06-30 15:47:17 +1200

    Make InputLabel's tertiaryLabel align to top

--- packages/lib/src/components/InputLabel/InputLabel.css.ts
+++ packages/lib/src/components/InputLabel/InputLabel.css.ts
@@ -1,10 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-export const label = style({
-  display: 'flex',
-  justifyContent: 'space-between',
+export const tertiaryLabelButton = style({
+  alignSelf: 'flex-start',
 });
-
-export const tertiaryLabelButton = style({});
-
-export const headingWrapper = style({});
