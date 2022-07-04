9331815ede93c197990b82381aaf2c0e100ee881
Kyle Beattie <kyle.beattie@alphero.com>
2022-07-04 15:04:24 +1200

    Added `onOutsideClick` so modal can be closed by clicking on backdrop

--- packages/lib/src/components/Dialog/Root.tsx
+++ packages/lib/src/components/Dialog/Root.tsx
@@ -1,9 +1,9 @@
-import { PropsWithChildren, ReactElement, ReactNode } from 'react';
+import { PropsWithChildren, ReactElement, ReactNode, useRef } from 'react';
 import * as RadixDialog from '@radix-ui/react-dialog';
+import { useOutsideClick } from 'rooks';
 
 import { ThemeProvider } from '../ThemeProvider/ThemeProvider';
 import { Stack } from '../Stack/Stack';
-import { Heading } from '../Heading/Heading';
 import { Icon } from '../Icon/Icon';
 import { Column } from '../Columns/Column';
 import { Row } from '../Columns/Row';
@@ -11,6 +11,9 @@ import { Content } from './Content';
 import { Actions } from './Actions';
 import { useTheme } from '../ThemeProvider/ThemeContext';
 import { AllowedChildren } from '../AllowedChildren/AllowedChildren';
+import { Box } from '../Box/Box';
+
+import { useHeading } from '../../hooks/typography';
 
 import { IconType } from '../Icon/icons';
 
@@ -43,7 +46,14 @@ export const Root = ({
   trigger,
   children,
 }: PropsWithChildren<DialogProps>) => {
+  const ref = useRef(null);
   const theme = useTheme();
+  const headingClassName = useHeading({ level: '2' });
+  const subheadingClassName = useHeading({ level: '4' });
+
+  useOutsideClick(ref, () => {
+    onOpenChange?.(false);
+  });
 
   return (
     <RadixDialog.Root defaultOpen={defaultOpen} open={open} onOpenChange={onOpenChange}>
@@ -54,7 +64,7 @@ export const Root = ({
           <RadixDialog.Content className={styles.dialog}>
             <Row flexGrow={1}>
               <Column center columns={8}>
-                <div className={styles.content}>
+                <div className={styles.content} id="REFFFF" ref={ref}>
                   <Row offset>
                     <Column center columns={4}>
                       <RadixDialog.Close className={styles.closeButton}>
@@ -63,9 +73,15 @@ export const Root = ({
                       <Stack alignItems="center" display="flex" flexDirection="column" space="medium">
                         {!!icon && <Icon icon={icon} variant="decorativeIcons" />}
                         <RadixDialog.Title asChild>
-                          <Heading level="2">{heading}</Heading>
+                          <Box as="span" className={headingClassName}>
+                            {heading}
+                          </Box>
                         </RadixDialog.Title>
-                        {!!subheading && <Heading level="4">{subheading}</Heading>}
+                        {!!subheading && (
+                          <Box as="span" className={subheadingClassName}>
+                            {subheading}
+                          </Box>
+                        )}
                         <AllowedChildren
                           errorMessage="Only Dialog.Content and Dialog.Actions components are allowed as children of Dialog.Root"
                           types={[Content, Actions]}
