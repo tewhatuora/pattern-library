30e5b0868980427062df41c3f90b454afbc29745
Kyle Beattie <kyle.beattie@alphero.com>
2022-07-04 14:31:37 +1200

    Created `CountryDropdown` for `InputPhone`

---
+++ packages/lib/src/components/InputPhone/CountryDropdown.css.ts
@@ -0,0 +1,52 @@
+import { style, styleVariants } from '@vanilla-extract/css';
+
+import { vars } from '../../themes/vars.css';
+import { responsiveStyle } from '../../css/responsiveStyle';
+
+export const countryDropdown = style([
+  {
+    position: 'relative',
+    backgroundColor: vars.color.primary5,
+  },
+  responsiveStyle({
+    mobile: {
+      width: '8.4rem',
+    },
+    tablet: {
+      width: '11.6rem',
+    },
+  }),
+]);
+
+export const flagWrapperBase = style([
+  {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '4.4rem',
+    height: '100%',
+    left: vars.space.xsmall.tablet,
+    top: '0',
+    zIndex: '2',
+    backgroundColor: vars.color.primary5,
+  },
+  responsiveStyle({
+    mobile: {
+      width: '3.6rem',
+    },
+    tablet: {
+      width: '4.4rem',
+    },
+  }),
+]);
+
+export const flagWrapper = styleVariants({
+  base: [flagWrapperBase],
+  international: [
+    flagWrapperBase,
+    {
+      color: vars.color.primary100,
+    },
+  ],
+});
