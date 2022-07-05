935bf856cec44ebe7c7c3872fa9b22ff8180424b
Kyle Beattie <kyle.beattie@alphero.com>
2022-07-06 09:34:22 +1200

    WIP created Documentation page component for Storybook

---
+++ packages/lib/utils/DocsPage.tsx
@@ -0,0 +1,18 @@
+import { ArgsTable, Description, PRIMARY_STORY, Primary, Stories, Subtitle, Title } from '@storybook/addon-docs';
+
+const DocsPage = (props) => {
+  const Docs = props?.docs || null;
+  return (
+    <>
+      <Title />
+      <Subtitle />
+      <Docs />
+      <Description />
+      <Primary />
+      <ArgsTable story={PRIMARY_STORY} />
+      <Stories />
+    </>
+  );
+};
+
+export default DocsPage;
