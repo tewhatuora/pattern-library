0338b326f185bfde5f792024deb502b83153d94a
Chris Meekin <chris.meekin@alphero.com>
2022-07-07 11:26:06 +1200

    Add Tabs controlled story

--- packages/lib/src/components/Tabs/Tabs.stories.tsx
+++ packages/lib/src/components/Tabs/Tabs.stories.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
+
 import { TabsRootProps } from './Root';
 import { Tabs } from './Tabs';
 
 export default {
   title: 'Components/Tabs',
-  component: Tabs,
+  component: Tabs.Root,
   argTypes: {
     onValueChange: {
       control: { type: 'function' },
@@ -32,7 +34,7 @@ export default {
   },
 };
 
-export const Default = (args: TabsRootProps) => (
+export const Uncontrolled = (args: TabsRootProps) => (
   <Tabs.Root {...args}>
     <Tabs.List>
       <Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
@@ -55,3 +57,31 @@ export const Default = (args: TabsRootProps) => (
     </Tabs.Content>
   </Tabs.Root>
 );
+
+export const Controlled = (args: TabsRootProps) => {
+  const [currentTab, setCurrentTab] = useState('tab-1');
+
+  return (
+    <Tabs.Root {...args} value={currentTab} onValueChange={setCurrentTab}>
+      <Tabs.List>
+        <Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
+        <Tabs.Trigger icon="tick" iconPosition="left" value="tab-2">
+          Tab 2
+        </Tabs.Trigger>
+        <Tabs.Trigger icon="document" iconPosition="right" value="tab-3">
+          Tab 3
+        </Tabs.Trigger>
+      </Tabs.List>
+
+      <Tabs.Content value="tab-1">
+        <p>Tab 1 content</p>
+      </Tabs.Content>
+      <Tabs.Content value="tab-2">
+        <p>Tab 2 content</p>
+      </Tabs.Content>
+      <Tabs.Content value="tab-3">
+        <p>Tab 3 content</p>
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+};
