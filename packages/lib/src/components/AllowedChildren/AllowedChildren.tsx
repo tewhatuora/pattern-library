1c7d81517ef09c702af33bb338fc26baeacc4463
Kyle Beattie <kyle.beattie@alphero.com>
2022-07-14 17:31:20 +1200

    Added `propsForChild` to `AllowedChildren` so that props can be applied to the passed in children.
    Created hook `useAllowedChildren` so it can be used to assert that allowed children are passed in, but can be rendered differently (wrapped in something else, for example)

--- packages/lib/src/components/AllowedChildren/AllowedChildren.tsx
+++ packages/lib/src/components/AllowedChildren/AllowedChildren.tsx
@@ -1,24 +1,63 @@
-import { Children, PropsWithChildren, ReactNode, cloneElement, isValidElement } from 'react';
+import { Children, PropsWithChildren, ReactChild, ReactNode, cloneElement, isValidElement } from 'react';
+
+import assert from 'assert';
 
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
-import assert from 'assert';
 
 type AllowedChildrenProps = {
   types: ReactNode[];
   errorMessage: string;
+  propsForChild?: (child?: ReactChild) => any;
 };
 
 /**
  * Only render allowed children of `type`
  * @param {Array<ReactNode>} types List of allowed children types
  * @param {string} errorMessage Error message when children are not allowed
+ * @param {Function} propsForChild Function that returns props for each child
  * @param {Array<ReactNode>} children Children
  * @constructor
  */
-export const AllowedChildren = ({ types, errorMessage, children }: PropsWithChildren<AllowedChildrenProps>) => {
-  const allowedChildren = Children.map(children, (child: ReactNode) => {
-    assert(isValidElement(child) && types.some((allowedType) => child.type === allowedType), errorMessage);
-    return cloneElement(child);
+export const useAllowedChildren = ({
+  types,
+  errorMessage,
+  propsForChild,
+  children,
+}: PropsWithChildren<AllowedChildrenProps>) => {
+  if (!children) return null;
+
+  return Children.map(children, (child: ReactNode) => {
+    assert(
+      isValidElement(child) && types.some((allowedType) => child.type === allowedType),
+      `${errorMessage}. ${child?.type} given`,
+    );
+
+    const props = propsForChild ? propsForChild?.(child) : null;
+
+    return cloneElement(child, props);
   });
+};
+
+/**
+ * Only render allowed children of `type` as component
+ * @param {Array<ReactNode>} types List of allowed children types
+ * @param {string} errorMessage Error message when children are not allowed
+ * @param {Function} propsForChild Function that returns props for each child
+ * @param {Array<ReactNode>} children Children
+ * @constructor
+ */
+export const AllowedChildren = ({
+  types,
+  errorMessage,
+  propsForChild,
+  children,
+}: PropsWithChildren<AllowedChildrenProps>) => {
+  const allowedChildren = useAllowedChildren({
+    types,
+    errorMessage,
+    propsForChild,
+    children,
+  });
+
   return <ErrorBoundary>{allowedChildren}</ErrorBoundary>;
 };
