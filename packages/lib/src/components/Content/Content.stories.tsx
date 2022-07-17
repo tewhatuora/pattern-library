ab093711244ee2a20e82f00deea4a3c2e5744d1e
Chris Meekin <chris.meekin@alphero.com>
2022-07-18 09:55:05 +1200

    Add heading levels to Content

--- packages/lib/src/components/Content/Content.stories.tsx
+++ packages/lib/src/components/Content/Content.stories.tsx
@@ -1,14 +1,10 @@
-import { Content } from './Content';
+import { Content, ContentProps } from './Content';
 
 export default {
   title: 'Components/Content',
   component: Content,
 };
 
-export const Default = () => {
-  return (
-    <Content heading="Heading" headingLevel="2" subheading="Subheading">
-      Content
-    </Content>
-  );
+export const Default = (args: ContentProps) => {
+  return <Content {...args}>Content</Content>;
 };
--- packages/lib/src/components/Content/Content.tsx
+++ packages/lib/src/components/Content/Content.tsx
@@ -3,9 +3,11 @@ import { PropsWithChildren } from 'react';
 import { Heading } from '../Heading/Heading';
 import { Text } from '../Text/Text';
 
-type ContentProps = {
+type ContentHeadingLevel = '1' | '2' | '3' | '4' | '5';
+
+export type ContentProps = {
   heading: string;
-  headingLevel: '1' | '2' | '3' | '4';
+  headingLevel: ContentHeadingLevel;
   subheading?: string;
   variant?: 'light' | 'dark';
 };
@@ -16,13 +18,31 @@ type ContentProps = {
  * @constructor
  */
 export const Content = ({ heading, headingLevel, subheading, children }: PropsWithChildren<ContentProps>) => {
+  const subheadingLevel = subheadingLevelForHeadingLevel[headingLevel];
+
   return (
     <>
-      <Heading level={headingLevel}>{heading}</Heading>
-      <Heading as="div" level="4">
-        {subheading}
-      </Heading>
+      {headingLevel === '5' ? <Text as="h5">{heading}</Text> : <Heading level={headingLevel}>{heading}</Heading>}
+
+      {subheadingLevel === '5' ? (
+        <Text as="div" weight="bold">
+          {heading}
+        </Text>
+      ) : (
+        <Heading as="div" level={subheadingLevel}>
+          {subheading}
+        </Heading>
+      )}
+
       <Text>{children}</Text>
     </>
   );
+};
+
+const subheadingLevelForHeadingLevel: Record<ContentHeadingLevel, ContentHeadingLevel> = {
+  '1': '3',
+  '2': '4',
+  '3': '4',
+  '4': '5',
+  '5': '5',
 };
