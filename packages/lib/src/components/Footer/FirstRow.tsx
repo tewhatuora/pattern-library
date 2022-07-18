ee9f1a3621d80731d82febb0d3d80fe9a4ff83f2
Chris Meekin <chris.meekin@alphero.com>
2022-07-19 10:33:28 +1200

    Progress Footer.FirstRow

--- packages/lib/src/components/Footer/FirstRow.tsx
+++ packages/lib/src/components/Footer/FirstRow.tsx
@@ -1,16 +1,23 @@
+import { PropsWithChildren } from 'react';
+
 import { Box } from '../Box/Box';
 
 import * as styles from './Footer.css';
 
 import NZGovtLogo from './nz-govt-logo.svg?component';
 
-export const FirstRow = () => {
+type FooterFirstRowProps = Record<string, unknown>;
+
+export const FirstRow = ({ children }: PropsWithChildren<FooterFirstRowProps>) => {
   return (
     <Box className={styles.firstRow} display="flex" justifyContent="spaceBetween">
       <Box className={styles.govtLogoWrapper}>
         <NZGovtLogo key={null} props={{}} ref={null} type="symbol" />
       </Box>
-      <Box className={styles.otherLogosWrapper}>other logos</Box>
+      <Box className={styles.otherLogosWrapper}>
+        {/* Allowed children: logos or images of some sort */}
+        {children}
+      </Box>
     </Box>
   );
 };
