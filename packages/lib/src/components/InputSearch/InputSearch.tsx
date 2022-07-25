d8ec71615de5f9d18787ed1ef5ed79f21dca4660
Kyle Beattie <kyle.beattie@alphero.com>
2022-07-25 17:12:23 +1200

    ForwardRef and use useTextField

--- packages/lib/src/components/InputSearch/InputSearch.tsx
+++ packages/lib/src/components/InputSearch/InputSearch.tsx
@@ -1,3 +1,6 @@
+import { ForwardedRef, RefObject, forwardRef } from 'react';
+import { useTextField } from '@react-aria/textfield';
+
 import { InputField, OtherInputFieldProps } from '../InputField/InputField';
 import { Box } from '../Box/Box';
 import { Button } from '../Button/Button';
@@ -8,27 +11,49 @@ export const InputSearchStyles = styles;
 
 export type InputSearchProps = Omit<OtherInputFieldProps, 'error' | 'disabled' | 'required'>;
 
+const INPUT_TYPE = 'search';
+
 /**
  * On search, leads to search results page.
  * @constructor
  */
-export const InputSearch = ({ id, name, placeholder, value, defaultValue, onChange }: InputSearchProps) => {
-  return (
-    <Box className={styles.container} display="flex" flexDirection="row">
-      <InputField
-        className={styles.input}
-        clearable
-        defaultValue={defaultValue}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        type="search"
-        value={value}
-        onChange={onChange}
-      />
-      <Button className={styles.button} icon="search" type="submit" variant="primary" />
-    </Box>
-  );
-};
+export const InputSearch = forwardRef<HTMLInputElement, InputSearchProps>(
+  (
+    { id, name, placeholder, value, defaultValue, onChange, ...rest }: InputSearchProps,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    const { inputProps } = useTextField(
+      {
+        id,
+        name,
+        placeholder,
+        value,
+        defaultValue,
+        ...rest,
+        type: INPUT_TYPE,
+      },
+      ref as RefObject<HTMLInputElement>,
+    );
+
+    return (
+      <Box className={styles.container}>
+        <InputField
+          {...inputProps}
+          className={styles.input}
+          clearable
+          defaultValue={defaultValue}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          ref={ref}
+          type={INPUT_TYPE}
+          value={value}
+          onChange={onChange}
+        />
+        <Button className={styles.button} icon="search" type="submit" variant="primary" />
+      </Box>
+    );
+  },
+);
 
 InputSearch.displayName = 'InputSearch';
