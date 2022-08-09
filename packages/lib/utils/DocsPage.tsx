ccfc3542b910d057074b9abfee8f8823defdfd53
Kyle Beattie <kyle.beattie@alphero.com>
2022-08-10 11:41:20 +1200

    Docs page styles

--- packages/lib/utils/DocsPage.tsx
+++ packages/lib/utils/DocsPage.tsx
@@ -4,11 +4,13 @@ const DocsPage = (props) => {
   const Docs = props?.docs || null;
   return (
     <>
-      <Title />
-      <Subtitle />
-      <Docs />
-      <hr />
-      <Description />
+      <div className="anatomic__docs">
+        <Title />
+        <Subtitle />
+        <Docs />
+        <hr />
+        <Description />
+      </div>
       <Primary />
       <ArgsTable story={PRIMARY_STORY} />
       <Stories />
