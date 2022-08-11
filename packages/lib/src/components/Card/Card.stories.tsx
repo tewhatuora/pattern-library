8822af70a7e554bbdb9096a07c0ffb468fda1cfb
Kyle Beattie <kyle.beattie@alphero.com>
2022-08-11 18:14:12 +1200

    Added variant control to Card stories

--- packages/lib/src/components/Card/Card.stories.tsx
+++ packages/lib/src/components/Card/Card.stories.tsx
@@ -20,6 +20,13 @@ export default {
         type: 'text',
       },
     },
+    variant: {
+      control: {
+        type: 'radio',
+        options: ['light', 'dark'],
+      },
+      defaultValue: 'light',
+    },
   },
   parameters: {
     docs: {
@@ -29,77 +36,71 @@ export default {
 };
 
 export const Single = (args: CardProps) => {
-  return <Card {...args} />;
-};
-Single.args = {
-  noShadow: false,
-  children: (
-    <>
-      <Text>
+  return (
+    <Card {...args}>
+      <Text color={args.variant === 'dark' ? 'primary0' : 'primary100'}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit nisi, rhoncus non nisi eu, gravida dictum ex.
         Nam posuere erat eu dui fringilla congue. Sed nunc magna, placerat in vehicula ut, lobortis a mauris. Morbi
         lacinia ex eu tortor vulputate ultricies. In lectus nulla, mattis at neque id, imperdiet fringilla arcu. In in
         risus pulvinar enim sollicitudin tempus.
       </Text>
-      <Text>
+      <Text color={args.variant === 'dark' ? 'primary0' : 'primary100'}>
         Aliquam erat volutpat. Pellentesque consequat erat ante, sed cursus augue dapibus at. Donec mauris nisl, euismod
         eu arcu vel, rutrum suscipit massa. Duis placerat bibendum lobortis. Integer mollis erat a odio facilisis, ac
         tempor nunc posuere. Phasellus tristique arcu id nulla scelerisque, non vestibulum velit venenatis.
       </Text>
-    </>
-  ),
+    </Card>
+  );
 };
 
 export const GridLayout = (args: CardProps) => {
+  const child = (
+    <Text color={args.variant === 'dark' ? 'primary0' : 'primary100'}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit nisi, rhoncus non nisi eu, gravida dictum ex.
+      Nam posuere erat eu dui fringilla congue.
+    </Text>
+  );
+
   return (
     <Container>
       <Row>
         <Column columns={12}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
       </Row>
       <Row>
         <Column columns={6}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={6}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
       </Row>
       <Row>
         <Column columns={4}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={4}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={4}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
       </Row>
       <Row>
         <Column columns={3}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={3}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={3}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
         <Column columns={3}>
-          <Card {...args} />
+          <Card {...args}>{child}</Card>
         </Column>
       </Row>
     </Container>
   );
-};
-GridLayout.args = {
-  noShadow: false,
-  children: (
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit nisi, rhoncus non nisi eu, gravida dictum ex.
-      Nam posuere erat eu dui fringilla congue.
-    </Text>
-  ),
 };
