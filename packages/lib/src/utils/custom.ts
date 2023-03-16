be817289b0c87545295ac26b72e07899f8430a2d
Dallan Freemantle <dallan@alphero.com>
2023-03-16 17:21:19 +1300

    Add comments

--- packages/lib/src/utils/custom.ts
+++ packages/lib/src/utils/custom.ts
@@ -2,6 +2,8 @@ import { vars } from '../themes/vars.css';
 
 export const focusColor = vars.color.annotation110;
 export const focusOutline = `${vars.borderWidth.xlarge} solid ${focusColor}`;
+// This is a hack to only show the focus ring when using keyboard nav
+// This is progressively enhanced so if :focus-visible isn't supported it falls back to :focus
 export const focusSelectorsStyles = {
   '&:focus': {
     outline: focusOutline,
