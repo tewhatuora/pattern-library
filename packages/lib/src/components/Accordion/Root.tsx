7056756b4a343405476111f041ebf7b16bb3b07d
Dallan Freemantle <dallan@alphero.com>
2023-03-27 16:58:29 +1300

    Forward all props to accordion

--- packages/lib/src/components/Accordion/Root.tsx
+++ packages/lib/src/components/Accordion/Root.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, createContext } from 'react';
+import { ComponentPropsWithoutRef, PropsWithChildren, createContext } from 'react';
 import * as RadixAccordion from '@radix-ui/react-accordion';
 
 import { AllowedChildren } from '../AllowedChildren/AllowedChildren';
@@ -8,9 +8,9 @@ import { ContrastVariant } from '../../types';
 
 import * as styles from './Accordion.css';
 
-export type AccordionRootProps = {
-  /** Accordion type - single allows only one item open at a time*/
-  type: 'single' | 'multiple';
+export type AccordionRootProps = ComponentPropsWithoutRef<typeof RadixAccordion.Root> & {
+  // /** Accordion type - single allows only one item open at a time*/
+  // type: 'single' | 'multiple';
   /** Contrast variant for dark/light UI */
   variant?: ContrastVariant;
   /** Heading level */
@@ -26,9 +26,19 @@ export const AccordionContext = createContext({ headingLevel: 3 });
  *
  * @constructor
  */
-export const Root = ({ type, variant = 'light', headingLevel, children }: PropsWithChildren<AccordionRootProps>) => {
+export const Root = ({
+  variant = 'light',
+  headingLevel,
+  children,
+  ...props
+}: PropsWithChildren<AccordionRootProps>) => {
   return (
-    <RadixAccordion.Root className={styles.root[variant]} collapsible type={type}>
+    <RadixAccordion.Root
+      className={styles.root[variant]}
+      // @ts-expect-error this is fine
+      collapsible
+      {...props}
+    >
       <AccordionContext.Provider value={{ headingLevel }}>
         <AllowedChildren
           errorMessage="Only `Accordion.Item` components are allowed as children of `Accordion.Root`"
