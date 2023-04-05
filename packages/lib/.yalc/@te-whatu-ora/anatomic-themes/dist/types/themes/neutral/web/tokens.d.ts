1c52bf5c5b3a7df7c34c4aa914ce5d5fb1289b9a
Dallan Freemantle <dallan@alphero.com>
2023-04-05 13:35:09 +1200

    Update build

--- packages/lib/.yalc/@te-whatu-ora/anatomic-themes/dist/types/themes/neutral/web/tokens.d.ts
+++ packages/lib/.yalc/@te-whatu-ora/anatomic-themes/dist/types/themes/neutral/web/tokens.d.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 29 Mar 2023 01:57:34 GMT
+ * Generated on Wed, 05 Apr 2023 01:33:45 GMT
  */
 import { Tokens } from '../../../tokenType';
 export declare const tokens: Tokens;
