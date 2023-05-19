df1ae9221878462d93c09d9317cf7d035558d1ed
Chris McArdle <chris.mcardle@alphero.com>
2023-05-19 16:15:21 +1200

    Refactor: Text link button new underline and weight props

--- packages/lib/src/components/Button/Button.tsx
+++ packages/lib/src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 import { Text } from '../Text/Text';
 import { Icon } from '../Icon/Icon';
 import { ButtonRoot } from './ButtonRoot';
+import { UseTextProps } from '../../hooks/typography';
 
 import { IconType } from '../Icon/icons';
 import fontWeightForButton from '../../utils/fontWeightForButton';
@@ -21,6 +22,8 @@ type KeysUnder<T, K extends PropertyKey> = T extends object
 type ColorVariant = KeysUnder<styles.Variants, 'color'>;
 
 export type ButtonProps = {
+  /** Font weight for `<Text>` */
+  weight?: UseTextProps['weight'];
   /** Icon to display **/
   icon?: IconType;
   /** Where to position the icon */
@@ -55,6 +58,7 @@ export const Button = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement
     disabled,
     className,
     icon,
+    weight = fontWeightForButton(variant),
     iconPosition = 'right',
     href,
     onPress,
@@ -82,7 +86,7 @@ export const Button = forwardRef((props: ButtonProps, ref: Ref<HTMLButtonElement
       )}
 
       {!!children && (
-        <Text size="medium" weight={fontWeightForButton(variant)}>
+        <Text size="medium" weight={weight}>
           {children}
         </Text>
       )}
