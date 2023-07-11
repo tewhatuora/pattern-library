244110d9302604622e9b600032cb00ec49a59538
Dallan Freemantle <dallan@alphero.com>
2023-07-12 11:42:28 +1200

    Fix checkbox button from shrinking

--- packages/lib/src/components/Checkbox/Checkbox.css.ts
+++ packages/lib/src/components/Checkbox/Checkbox.css.ts
@@ -39,6 +39,7 @@ export const checkbox = style({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+  flexShrink: 0,
   cursor: 'pointer',
 
   // Will be overridden by the selectors below
