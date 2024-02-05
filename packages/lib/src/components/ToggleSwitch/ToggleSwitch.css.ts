0cc73b1d6f5b8be413f493ca4d2938151d04b360
Dallan Freemantle <dallan@alphero.com>
2024-02-05 16:07:42 +1300

    feat(ToggleSwitch): add `ToggleSwitch` semantic tokens

--- packages/lib/src/components/ToggleSwitch/ToggleSwitch.css.ts
+++ packages/lib/src/components/ToggleSwitch/ToggleSwitch.css.ts
@@ -26,21 +26,21 @@ export const switchRoot = style({
   all: 'unset',
   width: rem(51),
   height: rem(31),
-  backgroundColor: vars.color.tertiary75,
+  backgroundColor: vars.color.semantic.controls.background.inactive,
   borderRadius: vars.borderRadius.topLeft.tags,
   position: 'relative',
   WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
   selectors: {
     '&[data-state=checked]': {
-      backgroundColor: vars.color.neutral100,
+      backgroundColor: vars.color.semantic.controls.background.active,
     },
     '&:disabled': {
       cursor: 'not-allowed',
-      backgroundColor: vars.color.tertiary25,
+      backgroundColor: vars.color.semantic.inputs.elements.background.disabled,
     },
     '&[data-state=checked]:disabled': {
       cursor: 'not-allowed',
-      backgroundColor: vars.color.tertiary50,
+      backgroundColor: vars.color.semantic.inputs.elements.background.disabled,
     },
   },
 });
@@ -50,7 +50,7 @@ export const switchThumb = style([
     width: rem(27),
     height: rem(27),
     display: 'block',
-    backgroundColor: vars.color.neutral0,
+    backgroundColor: vars.color.semantic.controls.background.light,
     borderRadius: vars.borderRadiusAll.tags,
     transition: 'transform 100ms ease-out',
     transform: `translateX(${rem(2.5)})`,
