0b861ab0ade9337d976aa5358f52ff3f26aa8387
Dallan Freemantle <dallan@alphero.com>
2024-02-05 15:50:43 +1300

    feat(InputDropdown): add `InputDropdown` semantic tokens

--- packages/lib/src/components/InputDropdown/InputDropdown.css.ts
+++ packages/lib/src/components/InputDropdown/InputDropdown.css.ts
@@ -10,7 +10,7 @@ export const chevron = style([
     position: 'absolute',
     top: '50%',
     transform: 'translateY(-50%)',
-    color: vars.color.primary100,
+    color: vars.color.semantic.icons.dark,
     cursor: 'pointer',
     zIndex: 1,
   },
@@ -31,7 +31,7 @@ export const placeholder = style([
     lineHeight: vars.space.xxlarge.tablet,
     paddingLeft: vars.space.xsmall.tablet,
     paddingRight: vars.space.xsmall.tablet,
-    color: vars.color.neutral50,
+    color: vars.color.semantic.text.copy.placeholder,
   },
   responsiveStyle({
     mobile: {
--- packages/lib/src/components/InputDropdown/InputDropdown.tsx
+++ packages/lib/src/components/InputDropdown/InputDropdown.tsx
@@ -8,8 +8,8 @@ import { InputMessage, InputMessageProps } from '../InputMessage/InputMessage';
 import { Icon } from '../Icon/Icon';
 import { Text } from '../Text/Text';
 
-import * as styles from './InputDropdown.css';
 import * as fieldStyles from '../InputField/InputField.css';
+import * as styles from './InputDropdown.css';
 
 import { useText } from '../../hooks/typography';
 
