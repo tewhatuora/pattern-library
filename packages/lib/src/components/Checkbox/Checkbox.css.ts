e38ebae95afbcfd7a73ce5318762db47982ded84
Kyle Beattie <kyle.beattie@alphero.com>
2024-05-03 10:10:54 +1200

    fix: reduce Checkbox size to 20px for consistency

--- packages/lib/src/components/Checkbox/Checkbox.css.ts
+++ packages/lib/src/components/Checkbox/Checkbox.css.ts
@@ -31,8 +31,8 @@ export const error = style({
 
 export const checkbox = style({
   all: 'unset',
-  width: rem(22),
-  height: rem(22),
+  width: rem(20),
+  height: rem(20),
   borderRadius: rem(4),
   borderStyle: 'solid',
   borderWidth: vars.borderWidth.medium,
