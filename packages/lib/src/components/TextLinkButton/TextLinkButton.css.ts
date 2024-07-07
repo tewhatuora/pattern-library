ae8b32965621e2bc7eec4161fbf92a94f441420a
Kyle Beattie <kyle.beattie@alphero.com>
2024-07-08 11:16:52 +1200

    fix: unset height form TextLinkButton

--- packages/lib/src/components/TextLinkButton/TextLinkButton.css.ts
+++ packages/lib/src/components/TextLinkButton/TextLinkButton.css.ts
@@ -26,7 +26,6 @@ export const link = recipe({
     display: 'inline-flex',
     alignItems: 'center',
     width: 'auto',
-    height: 'auto',
     color: vars.color.semantic.text.links.active,
     textDecoration: vars.textDecoration['link-normal'],
     cursor: 'pointer',
