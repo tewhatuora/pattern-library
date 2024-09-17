10eea03f547914d0fea73dfa0b144780d6016210
Kyle Beattie <kyle.beattie@alphero.com>
2024-09-18 10:35:22 +1200

    build: Upgrade Storybook & related dependencies

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,8 +1,37 @@
-import { addDecorator } from '@storybook/react';
-import { withThemes } from 'storybook-addon-themes/react';
-
+import React from 'react';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { ThemeProvider } from '../packages/lib/src';
+import { neutral, webSelfService } from '../packages/themes';
 import sbParameters from './parameters';
 
 export const parameters = sbParameters;
+export const tags = ['autodocs'];
 
-addDecorator(withThemes);
+const themes = {
+  neutral,
+  webSelfService,
+};
+
+const preview = {
+  decorators: [
+    (Story, options) => {
+      return (
+        <ThemeProvider theme={themes[options.globals.theme ?? themes.neutral]}>
+          <Story />
+        </ThemeProvider>
+      );
+    },
+    withThemeByClassName({
+      themes: Object.keys(themes).reduce(
+        (map, name) => ({
+          ...map,
+          [name]: themes[name].className,
+        }),
+        {},
+      ),
+      defaultTheme: neutral.className,
+    }),
+  ],
+};
+
+export default preview;
